df2b791a6f11395166c04a9770cff80206d5e145
Abel Misiocha <wells.mail.13@gmail.com>
2024-12-07 20:08:54 +0300

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
+import "./global.css"
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
 
 import { useColorScheme } from '@/components/useColorScheme';
 
